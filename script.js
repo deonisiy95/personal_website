2c10ce262fe7b6426af24e478b8859ec348962e0
denis <batalov@jivosite.com>
2023-01-03 17:19:10 +0500

    Add link

--- script.js
+++ script.js
@@ -62,6 +62,46 @@ function onKeyDown(event) {
   }
 }
 
+let xDown = null;
+let yDown = null;
+
+function handleTouchStart(event) {
+  const firstTouch = event.touches && event.touches[0];
+
+  if (!firstTouch) {
+    return;
+  }
+
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  const xUp = evt.touches[0].clientX;
+  const yUp = evt.touches[0].clientY;
+
+  const xDiff = xDown - xUp;
+  const yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if ( xDiff > 0 ) {
+      nextScreen();
+    } else {
+      previousScreen();
+    }
+  }
+
+  xDown = null;
+  yDown = null;
+}
+
 window.onload = function() {
-  document.addEventListener('keydown', onKeyDown)
+  document.addEventListener('keydown', onKeyDown);
+  document.addEventListener('click', nextScreen);
+  document.addEventListener('touchstart', handleTouchStart, false);
+  document.addEventListener('touchmove', handleTouchMove, false);
 }
